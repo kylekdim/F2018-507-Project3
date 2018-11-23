d86bdcaf5073240ae299c09eb67194a7923f4f20
Kyle <kylegomi@gmail.com>
2018-11-23 04:02:56 -0500

    added hw 10 code construct

--- proj3_choc.py
+++ proj3_choc.py
@@ -14,6 +14,82 @@ DBNAME = 'choc.db'
 BARSCSV = 'flavors_of_cacao_cleaned.csv'
 COUNTRIESJSON = 'countries.json'
 
+    try:
+        conn = sqlite3.connect(DBNAME)
+        cur = conn.cursor()
+    except:
+        print("error msg")
+
+        #drop tables like hw10
+
+        cur.execute(statement)
+    conn.commit()
+
+    statement = '''
+        DROP TABLE IF EXISTS 'Teams';
+    '''
+    cur.execute(statement)
+
+    statement = '''
+        DROP TABLE IF EXISTS 'Games';
+    '''
+    cur.execute(statement)
+
+    statement = '''
+        DROP TABLE IF EXISTS 'Rounds';
+    '''
+    cur.execute(statement)
+    conn.commit()
+    
+    # Create 'Teams' Table
+    statement = '''
+        CREATE TABLE 'Teams' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Seed' INTEGER NOT NULL,
+            'Name' TEXT NOT NULL,
+            'ConfRecord' TEXT NOT NULL
+        );
+    '''
+    try:
+        cur.execute(statement)
+    except:
+        print("Creation of Database Failed with 'Teams'. Please try again.")
+    conn.commit()
+
+    # Create 'Games' Table
+    statement = '''
+        CREATE TABLE 'Games' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Winner' INTEGER NOT NULL,
+            'Loser' INTEGER NOT NULL,
+            'WinnerScore' INTEGER NOT NULL,
+            'LoserScore' INTEGER NOT NULL,
+            'Round' INTEGER NOT NULL,
+            'Time' TEXT NOT NULL
+        );
+    '''
+    try:
+        cur.execute(statement)
+    except:
+        print("Creation of Database Failed with 'Games'. Please try again.")
+    conn.commit()
+
+    # Create 'Rounds' Table
+    statement = '''
+        CREATE TABLE 'Rounds' (
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Name' TEXT NOT NULL,
+            'Date' TEXT  NOT NULL
+        );
+    '''
+    try:
+        cur.execute(statement)
+    except:
+        print("Creation of Database Failed with 'Rounds'. Please try again.")
+    conn.commit()
+    conn.close()
+
+
 
 # Part 2: Implement logic to process user commands
 def process_command(command):
